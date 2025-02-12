9ab3ecc2d1fd29c7c587d35e68cf540dec41c038
Axel <ardungca23-0301@cca.edu.ph>
2025-02-12 17:56:15 +0800

    example
    for example

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>
+        <?php echo "hello World"; ?> <br>
+
+        <?php
+            $variable1 = 10;
+            $variable2 = 5;
+        ?>
+
+        Num1: <?php echo $variable1; ?> <br>
+        Num2: <?php echo $variable2; ?> <br>
+        Sum of Num1 and Num2: <?php echo $variable1 + $variable2; ?> <br>
+        </h1>
+    </body>
+</html>
